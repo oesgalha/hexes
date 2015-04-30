45dea3471fb4659a2f229c6d658b23a46549c415
oesgalha <oscaresgalha@gmail.com>
2015-04-30 01:44:41 -0300

    Updating hello world

--- samples/hello_world.rb
+++ samples/hello_world.rb
@@ -1,6 +1,9 @@
-Curses.initialize
-Curses.draw_panel("Hello World!", 0, 0, 30, 12)
-Curses.draw_panel("Uuuuuu", 40, 12, 30, 12)
-Curses.refresh
-Sleep::sleep(1)
-Curses.shutdown
+begin
+  Curses.initialize
+  Curses.draw_panel("Hello World!", 0, 0, 30, 12)
+  Curses.draw_panel("Uuuuuu", 40, 12, 30, 12)
+  Curses.refresh
+  Sleep::sleep(1)
+ensure
+  Curses.shutdown
+end
